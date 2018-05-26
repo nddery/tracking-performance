a4230f2cb15f39bfb9118d365fc41ef45987fb77
Nicolas Duvieusart Déry <nddery@gmail.com>
2018-05-26 15:26:02 -0400

    Add reveal.js notes plugin support

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import Reveal from 'reveal.js'
+import notesPluginUrl from 'reveal.js/plugin/notes/notes'
+import 'reveal.js/lib/js/head.min.js'
+import 'reveal.js/plugin/notes/notes.html'
 import 'reveal.js/css/reveal.css'
 import 'reveal.js/css/theme/serif.css'
 import './main.css'
 
 class Slideshow extends React.Component {
   componentDidMount() {
+    window.Reveal = Reveal
     Reveal.initialize({
       controlsTutorial: false,
       history: true,
       overview: false,
       transitionSpeed: 'fast',
+      dependencies: [{ src: notesPluginUrl, async: true }]
     })
   }
 
--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,13 @@ module.exports = {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: ['file-loader']
+      },
+      {
+        test: /reveal\.js\/plugin\/.*\.(js|html)$/,
+        use: {
+          loader: 'file-loader',
+          options: { name: '[path][name].[ext]' }
+        }
       }
     ]
   },
